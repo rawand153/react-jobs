263ab1af9d6c8945552b25bbb5e1245318bcf523
Rawand <rawand.rawand2030@gmail.com>
2024-07-13 10:34:54 +0300

    front-end done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 
 
@@ -27,7 +28,22 @@ const addJob = async (newJob) => {
 
   //Delete Job
   const deleteJob = async (id) => {
-    console.log('delete', id);
+    const response = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    })
+    return;
+  }
+
+  //Update Job
+  const updateJob = async (job) => {
+    const response = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    })
+    return;
   }
 
 const router = createBrowserRouter(
@@ -36,7 +52,14 @@ const router = createBrowserRouter(
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
       <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-      <Route path="/jobs/:id" element={<JobPage />} deleteJob={deleteJob} loader={jobLoader} />
+      <Route 
+      path="/jobs/:id" 
+      element={<JobPage deleteJob={deleteJob} />}  
+      loader={jobLoader} />
+      <Route 
+      path="/edit-job/:id" 
+      element={<EditJobPage updateJobSubmit={updateJob}/>}  
+      loader={jobLoader} />
       <Route path="*" element={<NotFoundPage />} />
 
       </Route>
